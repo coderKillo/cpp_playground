dcdd9febe173b94ebde443b28c7fceccd0c4f768
coderKillo <killo1234@web.de>
2019-02-06 00:23:18 +0100

    add Template Param

--- TemplateParam.cpp
+++ TemplateParam.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <string>
 
-
+template<class... Ts>
+struct Class
+{
+    template<typename T>
+    void print(const T& value)
+    {
+        std::cout << value << ", ";
+    }
+    
+    Class(Ts... args)
+    {
+        std::cout << "This contains" << std::endl;
+        auto x = {(print(args), 0)...};
+        std::cout << std::endl;
+    }
+};
 
 int main()
 {
-    
+    Class<int> x1(1);
+    Class<int,char> x2(2,'2');
+    Class<int,char, std::string> x3(2,'2', "test");
 }
