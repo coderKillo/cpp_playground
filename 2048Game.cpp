ec0c63e772ad541518f084f2bf6a3258228833d2
coderKillo <killo1234@web.de>
2019-02-20 23:56:24 +0100

    game done

--- 2048Game.cpp
+++ 2048Game.cpp
@@ -5,8 +5,9 @@
 #include <string>
 #include <random>
 #include <ctime>
+#include <vector>
 
-#define DEBUG
+//#define DEBUG
 
 constexpr uint8_t MAX_WIDTH = 4;
 constexpr uint8_t MAX_HEIGHT = 4;
@@ -17,10 +18,32 @@ void clrscr()
     std::cout << "\033[2J\033[1;1H";
 }
 
+struct Point
+{
+    Point(int px, int py):x(px),y(py){}
+    Point(int pos):x(pos / MAX_WIDTH), y(pos % MAX_WIDTH){}
+    friend std::ostream& operator<<(std::ostream& ostr, const Point& p)
+    {
+        ostr << "x: " << p.x << "y: " << p.y;
+        return ostr;
+    }
+    int x;
+    int y;
+};
 
 struct Tile
 {
     int value = 0;
+};
+
+class TileMap
+{
+public:
+    Tile& get(int x, int y){return m_TileMap[x][y];}
+    Tile& get(const Point& p){return m_TileMap[p.x][p.y];}
+
+private:
+    Tile m_TileMap[MAX_WIDTH][MAX_HEIGHT];
 };
 
 enum class Direction:int
@@ -69,7 +92,7 @@ public:
         std::cout << "|";
         for(int i = 0 ; i < MAX_WIDTH; ++i)
         {
-            std::cout << m_tileMap[i][line];
+            std::cout << m_tileMap.get(i, line);
             std::cout << "|";
         }
         std::cout << "\n";
@@ -87,22 +110,44 @@ public:
             drawLine('|');   
             drawLine('+');
         }
+        std::cout << "Keys: ^(W) <(A) V(S) >(D) quit(E)";
+    #ifdef DEBUG
+        std::cout << "\n##########\n DEBUG CONSOLE:\n";
+        std::cout << "last move: " << static_cast<int>(m_moveDir) << std::endl;
+        std::cout << "##########\n";
+    #endif // DEBUG
     }    
     void addTile()
     {
-        m_tileMap[0][3].value = 2;
+        std::vector<int> vec;
+        vec.reserve(MAX_WIDTH * MAX_HEIGHT);
+        for (int i = 0; i < MAX_WIDTH; i++)
+            for (int j = 0; j < MAX_HEIGHT; j++)
+                if(m_tileMap.get(i, j).value == 0)
+                    vec.push_back(i*MAX_WIDTH + j);
+        vec.shrink_to_fit();
+        
+        if(vec.empty())
+        {
+            m_gameover = true;
+            return;
+        }
+        
+        int rnd = rand();
+        int value = ((rnd % 2) != 0) ? 2 : 4;
+        int pos = vec[rnd % vec.size()];
+        std::cout << Point(pos) << std::endl;
+        m_tileMap.get(Point(pos)).value = value;
     }
 
-    bool moveTile(Tile& from, Tile& to)
+    void moveTile(Tile& from, Tile& to)
     {
-        if(from.value == 0)
-            return false;
         if(from.value != to.value && to.value != 0)
-            return false;
-        
+            return;
+        if(from.value == to.value) 
+            m_score += from.value;
         to.value += from.value;
         from.value = 0;
-        return true;
     }
 
     void move()
@@ -111,17 +156,23 @@ public:
         {
         case Direction::UP:
             for (int i = 0; i < MAX_WIDTH; ++i)
-                for(int j = MAX_HEIGHT - 1; j >= 0; --j)
-                    moveTile(m_tileMap[i][j],m_tileMap[i][j - 1]);
+                for(int j = MAX_HEIGHT - 1; j > 0; --j)
+                    moveTile(m_tileMap.get(i, j), m_tileMap.get(i, j-1));
             break;
         case Direction::DOWN:
-
+            for (int i = 0; i < MAX_WIDTH; ++i)
+                for(int j = 0; j < MAX_HEIGHT - 1; ++j)
+                    moveTile(m_tileMap.get(i, j), m_tileMap.get(i, j+1));
             break;
         case Direction::RIGHT:
-
+            for (int j = 0; j < MAX_HEIGHT; ++j)
+                for(int i = 0; i < MAX_WIDTH - 1; ++i)
+                    moveTile(m_tileMap.get(i, j), m_tileMap.get(i+1, j));
             break;
         case Direction::LEFT:
-
+            for (int j = 0; j < MAX_HEIGHT; ++j)
+                for(int i = MAX_WIDTH - 1; i > 0; --i)
+                    moveTile(m_tileMap.get(i, j), m_tileMap.get(i-1, j));
             break;
         
         default:
@@ -140,9 +191,9 @@ public:
             m_moveDir = Direction::UP;
         if(in == 's')
             m_moveDir = Direction::DOWN;
-        if(in == 'a')
-            m_moveDir = Direction::RIGHT;
         if(in == 'd')
+            m_moveDir = Direction::RIGHT;
+        if(in == 'a')
             m_moveDir = Direction::LEFT;
     }
 
@@ -152,20 +203,12 @@ public:
         draw();
         waitForInput();
         move();
-
-    #ifdef DEBUG
-        std::cout << "\n##########\n DEBUG CONSOLE:\n";
-        std::cout << "random Number:" << static_cast<float>(rand()) << std::endl;
-        std::cout << "last move: " << static_cast<int>(m_moveDir) << std::endl;
-        std::cout << "##########\n";
-    #endif // DEBUG
-
         return !m_gameover;
     }
 
 private:
 
-    Tile m_tileMap[MAX_WIDTH][MAX_HEIGHT];
+    TileMap m_tileMap;
     int m_score = 0;
     bool m_gameover = false;
     Direction m_moveDir = Direction::NONE;
@@ -178,5 +221,6 @@ int main(int argc, char const *argv[])
     while(game.Running())
     {
     }
+    std::cout << "You Lose!" << std::endl;
     return 0;
 }
